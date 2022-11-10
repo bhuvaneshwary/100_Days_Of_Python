cf9e9e206b3a4370118114e0321a44a3e665c57b
Bhuvaneshwary <54786817+bhuvaneshwary@users.noreply.github.com>
2022-11-10 07:47:57 +0530

    Delete Question8.py

--- Question8.py
+++
@@ -1,5 +0,0 @@
-height =  int(input("please enter your height in cms"))
-h_inches = height /2.54
-h_feet = h_inches/12
-print("your height in inches ", h_inches)
-print("your height in feet ", h_feet)
