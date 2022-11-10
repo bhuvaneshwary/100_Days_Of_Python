6e7b88e0a06600c102fa20d154fdeff66960c283
Bhuvaneshwary <54786817+bhuvaneshwary@users.noreply.github.com>
2022-11-10 07:48:23 +0530

    Delete Question10.py

--- Question10.py
+++
@@ -1,5 +0,0 @@
-#program to find the area of a trainge 
-l = int(input("Please enter the length of a triangle"))
-b = int(input("Please enter the breadth of the traingle"))
-ar_of_triange =  1/2*l*b 
-print("area of triangle is : ", ar_of_triange)
